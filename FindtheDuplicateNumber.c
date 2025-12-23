27cb64b185ffb984b3ff3faac3a441b8e03113a7
Pulkit Singh <singhpulkit1506@gmail.com>
2025-12-24 01:20:06 +0530

    Problem 1

---
+++ FindtheDuplicateNumber.c
@@ -0,0 +1,40 @@
+/*
+287. Find the Duplicate Number
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and using only constant extra space.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [3,3,3,3,3]
+Output: 3
+*/
+
+int findDuplicate(int* nums, int numsSize) {
+    int slow = nums[0];
+    int fast = nums[0];
+    while (1) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (slow == fast)
+            break;
+    }
+
+    slow = nums[0];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+}
