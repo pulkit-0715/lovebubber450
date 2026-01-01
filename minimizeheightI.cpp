e497db2208f4bbcf30c3f319a517bfb4e45a32f6
pulkit-0715 <singhpulkit1506@gmail.com>
2026-01-01 20:56:23 +0530

    Problem added

---
+++ minimizeheightI.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+  public:
+    int getMinDiff(vector<int> &arr, int k) {
+         int n = arr.size();
+        if (n == 1) return 0;
+
+        sort(arr.begin(), arr.end());
+
+        int ans = arr[n - 1] - arr[0];
+
+        for (int i = 1; i < n; i++) {
+      
+
+            int minimum = min(arr[0] + k, arr[i] - k);
+            int maximum = max(arr[i - 1] + k, arr[n - 1] - k);
+
+            ans = min(ans, maximum - minimum);
+        }
+
+        return ans;
+        
+    }
+};
