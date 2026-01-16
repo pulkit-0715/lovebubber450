8bddd7164d6f327d2429331ddda3d6309c1016c4
pulkit-0715 <singhpulkit1506@gmail.com>
2026-01-16 23:57:10 +0530

    linkedlist

---
+++ linkedlist/removalofloopinll.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+void removeLoop(struct Node* head) {
+    struct Node* slow = head;
+    struct Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            break;
+    }
+
+    if (slow != fast)
+        return;
+
+    slow = head;
+    while (slow->next != fast->next) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    fast->next = NULL;
+}
