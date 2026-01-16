d35cfcf1b5d31ceeb853cb6675723d98a392e75e
pulkit-0715 <singhpulkit1506@gmail.com>
2026-01-16 23:43:22 +0530

    linkedlist

---
+++ linkedlist/loopinll.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int detectLoop(struct Node* head) {
+    struct Node* slow = head;
+    struct Node* fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return 1;
+    }
+    return 0;
+}
